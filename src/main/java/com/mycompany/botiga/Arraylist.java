24d291e5d7edd8f397acc4e69e4e536e77c1a8d8
Enmanuel <pineda3201@gmail.com>
2024-01-09 19:50:04 +0100

    Atributos productos

---
+++ src/main/java/com/mycompany/botiga/Arraylist.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.botiga;
+
+/**
+ *
+ * @author enmanuel.pineus.dam_
+ */
+class Arraylist {
+    
+}
